6d7952b7f392bf332b840004c1061e953108fe2d
m.ahmedmedo <m.ahmedmedo457@gmail.com>
2021-02-14 11:22:06 +0200

    save facebook login user

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
-
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 class SocialController extends Controller
 {
     public function redirect($service) {
@@ -13,7 +14,25 @@ class SocialController extends Controller
 
     public function callback($service) {
        $user = Socialite::with($service) -> user();
-       return response()->json($user);
+    //    $user =
+    //    [
+    //    "name"=>"Mahmoud",
+    //    "avatar"=>"https:\/\/graph.facebook.com\/v3.3\/3582631791835701\/picture?type=normal",
+    //    "name"=>"Mahmoud",
+    //    "email"=>"mahmoudrabie401@yahoo.com",
+    //    "avatar_original"=>"https:\/\/graph.facebook.com\/v3.3\/3582631791835701\/picture?width=1920",
+    //     "profileUrl"=>null];
+        $create = User::create([
+            "username" => $user->name,
+            "name" => $user->name,
+            "avatar" => $user->avatar,
+            "email" => $user->email,
+            'password' => Hash::make($user->name.$user->email),
+            'remember_token' => $user->token
+        ]);
+        // dd($create);
+
+       return response()->json($create);
 
     }
 }
